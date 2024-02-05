30adb19b70417ced0b197e084052153a5521b4ac
jonathan <jonathan.123kutumbakana@gmail.com>
2024-02-05 09:50:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AccountContainer from "./components/AccountContainer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div className="ui raised segment">
+        <div className="ui segment violet inverted">
+          <h2>The Royal Bank of Flatiron</h2>
+        </div>
+        <AccountContainer />
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
---
+++ src/components/AccountContainer.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+import AddTransactionForm from "./AddtranscationFrom";
+
+function AccountContainer() {
+  const [search, setSearch] = useState("");
+  const [transactions, setTransactions] = useState([]);
+  return (
+    <div>
+      <Search Search={search} setSearch={setSearch} />
+      <AddTransactionForm transactions={transactions} setTransactions={setTransactions} />
+      <TransactionsList transactions={transactions} setTransactions={setTransactions} search={search} setSearch={setSearch} />
+    </div>
+  );
+}
+
+
+export default AccountContainer;
